3d51d3222daf678b8f8387a9fff65ce209536b7a
Arthur Treluyer <a.treluyer@student.isartdigital.com>
2022-04-25 15:47:55 -0400

    PickUpScriptandCharaTests

---
+++ ThunderBird/Assets/Scripts/PickUpItem.cs
@@ -0,0 +1,103 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PickUpItem : MonoBehaviour
+{
+    [Tooltip("Feature for one using only")]
+    public bool OneTime = false;
+    [Tooltip("Plug follow this local EmptyObject")]
+    public Transform HeroHandsPosition;
+    [Tooltip("SocketObject with collider(shpere, box etc.) (is trigger = true)")]
+    public Collider Socket; // need Trigger
+
+    // NearView()
+    float distance;
+    float angleView;
+    Vector3 direction;
+
+    bool follow = false, isConnected = false, followFlag = false, youCan = true;
+    Rigidbody rb;
+
+    public GameObject player;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+        if (youCan) Interaction();
+
+        // frozen if it is connected to PowerOut
+        if (isConnected)
+        {
+            gameObject.transform.position = Socket.transform.position;
+            gameObject.transform.rotation = Socket.transform.rotation;
+        }
+        else
+        {
+            return;
+        }
+    }
+
+    void Interaction()
+    {
+        if (NearView() && Input.GetKeyDown(KeyCode.E) && !follow)
+        {
+            isConnected = false; // unfrozen
+            follow = true;
+            followFlag = false;
+        }
+
+        if (follow)
+        {
+            rb.drag = 10f;
+            rb.angularDrag = 10f;
+            if (followFlag)
+            {
+                distance = Vector3.Distance(transform.position, player.transform.position);
+                if (distance > 3f && Input.GetKeyDown(KeyCode.E))
+                {
+                    follow = false;
+                }
+            }
+
+            followFlag = true;
+            rb.AddExplosionForce(-1000f, HeroHandsPosition.position, 10f);
+            // second variant of following
+            //gameObject.transform.position = Vector3.Lerp(gameObject.transform.position, objectLerp.position, 1f);
+        }
+        else
+        {
+            rb.drag = 0f;
+            rb.angularDrag = .5f;
+        }
+    }
+
+    bool NearView() // it is true if you near interactive object
+    {
+        distance = Vector3.Distance(transform.position, player.transform.position);
+        //direction = transform.position - Camera.main.transform.position;
+        //angleView = Vector3.Angle(Camera.main.transform.forward, direction);
+
+        if (distance < 10)
+        {
+            print("Near");
+            print(follow);
+            return true;
+        }
+        else return false;
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other == Socket)
+        {
+            isConnected = true;
+            follow = false;
+        }
+        if (OneTime) youCan = false;
+    }
+}
