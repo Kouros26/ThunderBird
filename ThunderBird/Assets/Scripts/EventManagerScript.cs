e9444aea56bc36827e4b442015ca0530850fb33c
Arthur Treluyer <a.treluyer@student.isartdigital.com>
2022-04-27 19:58:40 -0400

    EventManager & UI adjustments in Arthur Scene

--- ThunderBird/Assets/Scripts/EventManagerScript.cs
+++ ThunderBird/Assets/Scripts/EventManagerScript.cs
@@ -12,39 +12,90 @@ public class EventManagerScript : MonoBehaviour
     public GameObject Shake;
     public GameObject Air;
 
+    bool specialTask = false;
+    bool task = false;
+    bool alreadyFire = false;
+    bool alreadyFuel = false;
+    bool alreadyDoor = false;
+    bool alreadyPet = false;
+
     void Update()
     {
-        if(isEventThunderOn == true)
+        if (!specialTask)
         {
-            Thunder.SetActive(true);
-            Shake.SetActive(false);
-            Air.SetActive(false);
-        }
-        else
-        {
-            Thunder.SetActive(false);
+            specialTask = true;
+            StartCoroutine(WaitForSpecialMission());
         }
 
-        if (isEventShakeOn == true)
+        if (!task)
         {
-            Shake.SetActive(true);
-            Thunder.SetActive(false);
-            Air.SetActive(false);
+            StartCoroutine(WaitForMission());
         }
-        else
-        {
-            Shake.SetActive(false);
-        }
+    }
 
-        if (isEventAirOn == true)
+    IEnumerator WaitForSpecialMission()
+    {
+        int random = Random.Range(40, 91);
+        yield return new WaitForSeconds(random);
+        int eventToPerform = Random.Range(1, 4);
+        if(eventToPerform == 1)
         {
-            Air.SetActive(true);
-            Thunder.SetActive(false);
-            Shake.SetActive(false);
+            Thunderstorm();
         }
-        else
+        else if(eventToPerform == 2)
         {
-            Air.SetActive(false);
+            Turbulences();
         }
+        else if(eventToPerform == 3)
+        {
+            Depressurization();
+        }
+    }
+    void Thunderstorm()
+    {
+        Thunder.SetActive(true);
+        Shake.SetActive(false);
+        Air.SetActive(false);
+    }
+
+    void Turbulences()
+    {
+        Shake.SetActive(true);
+        Thunder.SetActive(false);
+        Air.SetActive(false);
+    }
+
+    void Depressurization()
+    {
+        Air.SetActive(true);
+        Thunder.SetActive(false);
+        Shake.SetActive(false);
+    }
+
+    IEnumerator WaitForMission()
+    {
+        int random = Random.Range(8, 17);
+        yield return new WaitForSeconds(random);
+        int mission = Random.Range(1, 5);
+    }
+
+    void Fire()
+    {
+
+    }
+
+    void Fuel()
+    {
+
+    }
+
+    void PetTheDragon()
+    {
+
+    }
+
+    void DoorClose()
+    {
+
     }
 }
